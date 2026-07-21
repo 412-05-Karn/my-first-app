38e44b45604bc7710e7daf46e0e99715d4a8a5b8
412-05-Karn <54208@yupparaj.ac.th>
2026-07-21 11:03:53 +0700

    Fix variable name for year conversion

--- convertYear.py
+++ convertYear.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("เเอปพลิเคชั่นเเปลงปี พ.ศ. เป็น ค.ศ.")
 
 bh_yrar=st.number_input("กรอกปี พ.ศ. ที่ต้องการเเปลง", value=2569)
-ce_year=bhyear-543
-st.header(f"ปี ค.ศ. คือ : {ce_y}")
+ce_year=bh_year-543
+st.header(f"ปี ค.ศ. คือ : {ce_year}")
